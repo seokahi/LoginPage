494788d3d530863efe73cbdc24dbf5ff5706a7c8
seokahi <skh9797@naver.com>
2022-03-21 01:07:05 +0900

    feat : 로그인 페이지 프론트엔드

---
+++ SKH_LION.js
@@ -0,0 +1,16 @@
+function GoToGoogle() {
+    window.location="https://www.google.co.kr";
+}
+
+function GoToFacebook() {
+    window.location="https://www.facebook.com/";
+}
+
+function GoToNaver() {
+    window.location="https://www.naver.com/";
+}
+
+function GoToKakaoTalk() {
+    window.location="https://www.kakaocorp.com/page/service/service/KakaoTalk";
+}
+
